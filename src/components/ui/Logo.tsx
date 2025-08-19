11440fbaaafdb524da41085a74bff6adac23499d
Thrones <nestorcachacelmi@gmail.com>
2025-08-18 20:20:15 -0500

    Cambios en los colores

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
         <h1 className="text-3xl font-extrabold text-white">POS {''}
-            <span className="text-teal-400 text-xl ">Next.js / Nest</span>
+            <span className="text-teal-400 text-xl ">Sistema de venta</span>
         </h1>
     )
 }
--- src/components/ui/MainNav.tsx
+++ src/components/ui/MainNav.tsx
@@ -34,7 +34,7 @@ export default async function MainNav() {
 
                 <Link
                     href={'/admin/sales'}
-                    className="rounded bg-green-400 font-bold py-2 px-10"
+                    className="rounded bg-teal-400 font-bold py-2 px-10"
                 >Panel de Administración</Link>
             </nav>
         </header>
